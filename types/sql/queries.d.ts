b119e00344673066740ecb1da02f731bcff3a53e
Michael <michael8t6@gmail.com>
2024-04-18 16:39:11 +0100

    BREAKING: Refactored queries into child objects

--- types/sql/queries.d.ts
+++ types/sql/queries.d.ts
@@ -1,22 +1,29 @@
 // import PurchaseOrder
 
 export type Queries = {
-  fetchPrefixes: () => Promise<string[] | void>;
-  insertPrefix: (prefix: string) => Promise<true | void>;
-  insertDataToDb: (data: Data) => Promise<true | "ER_DUP_ENTRY" | void>;
-  fetchPurchaseOrders: () => Promise<string[] | void>;
-  fetchPurchaseOrder: (id: string) => Promise<PurchaseOrder | void>;
-  selectEmail: (email: string) => Promise<number | void>;
-  createUser: (email: string, password: string) => Promise<string | void>;
-  validateLogin: (email: string) => Promise<string[] | void>;
-  validateUserToken: (email: string, token: string) => Promise<boolean | void>;
-  setTokenToNull: (email: string, token: string) => Promise<true | void>;
-  updateUserToken: (email: string, token: string) => Promise<true | void>;
-  patchPartialStatus: (order: string, name: string) => Promise<true | void>;
-  addParcelsToOrder: (
-    parcels: number[],
-    purchaseOrder: string,
-    part: string
-  ) => Promise<true | void>;
-  getUserRole: (email: string) => Promise<number | void>;
+  parts: {
+    fetchPrefixes: () => Promise<string[] | void>;
+    insertPrefix: (prefix: string) => Promise<true | void>;
+  };
+  purchaseOrders: {
+    //Rename insert data to db
+    insertDataToDb: (data: Data) => Promise<true | "ER_DUP_ENTRY" | void>;
+    fetchPurchaseOrders: () => Promise<string[] | void>;
+    fetchPurchaseOrder: (id: string) => Promise<PurchaseOrder | void>;
+    patchPartialStatus: (order: string, name: string) => Promise<true | void>;
+    addParcelsToOrder: (
+      parcels: number[],
+      purchaseOrder: string,
+      part: string
+    ) => Promise<true | void>;
+  };
+  user: {
+    selectEmail: (email: string) => Promise<number | void>;
+    createUser: (email: string, password: string) => Promise<string | void>;
+    validateLogin: (email: string) => Promise<string[] | void>;
+    validateUserToken: (email: string, token: string) => Promise<boolean | void>;
+    setTokenToNull: (email: string, token: string) => Promise<true | void>;
+    updateUserToken: (email: string, token: string) => Promise<true | void>;
+    getUserRole: (email: string) => Promise<number | void>;
+  };
 };
