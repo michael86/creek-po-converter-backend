9e4f96455632d263914ed8f3bad3c0a510df9457
michael86 <michael8t6@gmail.com>
2024-04-22 14:18:43 +0100

    Refactored pdf upload to hook inot new queries scaffold

--- types/utils/index.d.ts
+++ types/utils/index.d.ts
@@ -1,3 +1,3 @@
 export type Rows = string[][];
-export type GetData = (rows: Rows) => Rows;
-export type ShouldIncludeString = (string: string, row: string[]) => boolean;
+export type GetData = (rows: Rows) => Promise<Rows>;
+export type ShouldIncludeString = (string: string, row: string[]) => Promise<boolean | undefined>;
