f649cec02900413ea240593aadd9993df5dd0fb5
michael86 <michael8t6@gmail.com>
2025-03-03 22:08:20 +0000

    begun extracting data from tables, work in progress

--- src/middleware/pdf.ts
+++ src/middleware/pdf.ts
@@ -1,21 +1,33 @@
 import { RequestHandler } from "express";
-import { PdfUpload } from "src/types/pdf";
+import { PdfUpload } from "../types/pdf";
 import path from "path";
 import fs from "fs/promises";
+import pdfParse from "pdf-parse";
+import { extractDataFromPDF, extractPurchaseAndOrderRef, extractTableData } from "../utils/pdf";
 
 const UPLOADS = path.resolve(__dirname, "../uploads");
 
 export const validatePDF: RequestHandler = async (req: PdfUpload, res, next) => {
-  const { file } = req;
+  try {
+    const { file } = req;
 
-  if (!file) {
-    res.status(400).json({ status: "error", message: "No file provided" });
+    if (!file) {
+      res.status(400).json({ status: "error", message: "No file provided" });
+      return;
+    }
+
+    const { path: filePath } = file;
+
+    const pdfBuffer = await fs.readFile(filePath);
+    const parsed = await pdfParse(pdfBuffer);
+
+    const data = await extractDataFromPDF(parsed.text.split("\n").map((line) => line.trim()));
+
+    console.log("data ", data);
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "error", message: "Internal server error" });
     return;
   }
-
-  const { path: filePath } = file;
-
-  const fileData = await fs.readFile(path.resolve(UPLOADS, filePath));
-
-  next();
 };
