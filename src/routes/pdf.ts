2b42b30001e9947c8ae356064a4462d0f2f8fbae
michael86 <michael8t6@gmail.com>
2025-03-06 20:28:38 +0000

    extracted description from pdf

--- src/routes/pdf.ts
+++ src/routes/pdf.ts
@@ -1,15 +1,9 @@
 import { Router } from "express";
+import { validatePDF } from "../middleware/pdf";
+import { insertPdf } from "../controllers/pdf";
 
 const route = Router();
 
-route.post("/upload", async (req, res, next) => {
-  try {
-    console.log("upload complete");
-    res.status(200).json({ status: "success", message: "pdf uploaded" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ status: "error", message: "internal server error" });
-  }
-});
+route.post("/upload", validatePDF, insertPdf);
 
 export default route;
