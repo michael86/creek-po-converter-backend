13dda193e37b36538037d8234da8005bc5d7de0a
michael86 <michael8t6@gmail.com>
2025-03-07 21:12:35 +0000

    Added insert purchase query

--- src/controllers/pdf.ts
+++ src/controllers/pdf.ts
@@ -1,18 +1,30 @@
 import { RequestHandler } from "express";
 import { PdfUpload } from "../types/pdf";
+import { insertPurchaseOrder } from "../queries/pdf";
 
-export const insertPdf: RequestHandler = async (req: PdfUpload, res, next) => {
+export const insertPdf: RequestHandler = async (req: PdfUpload, res) => {
   try {
     const { pdfData } = req;
     if (!pdfData) {
-      res.status(400).json({ status: "error", message: "No pdf provided" });
+      res.status(400).json({ status: "error", message: "No PDF provided" });
       return;
     }
 
     const { purchaseOrder, orderRef, data } = pdfData;
-    console.log(pdfData);
+
+    const purchaseId = await insertPurchaseOrder(purchaseOrder, orderRef);
+    if (!purchaseId) {
+      throw new Error("Error inserting purchase order");
+    }
+
+    console.log("Inserted Purchase Order ID:", purchaseId);
+
+    res.status(201).json({
+      status: "success",
+      message: "Purchase order inserted successfully",
+    });
   } catch (error) {
-    console.error("Error inserting pdf\n", error);
+    console.error("Error inserting PDF:\n", error);
     res.status(500).json({ status: "error", message: "Internal server error" });
     return;
   }
