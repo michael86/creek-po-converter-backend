a07e1200968657ea867fb66499a06a694d4b2937
michael86 <michael8t6@gmail.com>
2025-03-08 23:16:09 +0000

    select a complete po by uuid

--- src/controllers/purchaseOrders.ts
+++ src/controllers/purchaseOrders.ts
@@ -1,5 +1,9 @@
 import { Request, RequestHandler, Response } from "express";
-import { deletePurchaseOrderById, selectPurchaseOrderNames } from "../queries/purchaseOrders";
+import {
+  deletePurchaseOrderById,
+  selectPurchaseOrderByUuid,
+  selectPurchaseOrderNames,
+} from "../queries/purchaseOrders";
 
 export const deletePurchaseOrder = async (req: Request, res: Response) => {
   try {
@@ -26,6 +30,27 @@ export const handleNames: RequestHandler = async (req, res, next) => {
     if (!names) throw new Error("Failed to select all purchase order names");
 
     res.status(200).json({ status: "success", data: names });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "error", message: "Internale Server Error" });
+
+    return;
+  }
+};
+
+export const selectPurchaseOrder: RequestHandler = async (req, res, next) => {
+  try {
+    const { uuid } = req.params;
+    if (!uuid) {
+      res.status(400).json({ status: "error", message: "Uuid missing or invalid" });
+      return;
+    }
+
+    const data = await selectPurchaseOrderByUuid(uuid);
+
+    if (!data) throw new Error(`Failed to select purchase order for ${uuid}`);
+
+    res.status(200).json({ status: "success", data });
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: "error", message: "Internale Server Error" });
--- src/routes/purchaseOrders.ts
+++ src/routes/purchaseOrders.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { deletePurchaseOrder, handleNames } from "../controllers/purchaseOrders";
+import {
+  deletePurchaseOrder,
+  handleNames,
+  selectPurchaseOrder,
+} from "../controllers/purchaseOrders";
 import { validateDeleteParam } from "../middleware/purchaseOrders";
 
 const router = Router();
 
 router.delete("/delete/:id", validateDeleteParam, deletePurchaseOrder);
 router.get("/names", handleNames);
-router.get("/:uuid");
+router.get("/:uuid", selectPurchaseOrder);
 
 export default router;
