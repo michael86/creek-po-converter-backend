4e0f851233db0e5018f53558568d371d4057c079
michael86 <michael8t6@gmail.com>
2025-03-08 21:23:06 +0000

    moved multer to pdf route

--- src/routes/pdf.ts
+++ src/routes/pdf.ts
@@ -1,9 +1,12 @@
+import multer from "multer";
+const upload = multer({ dest: "uploads/" });
+
 import { Router } from "express";
 import { validatePDF } from "../middleware/pdf";
 import { insertPdf } from "../controllers/pdf";
 
 const route = Router();
 
-route.post("/upload", validatePDF, insertPdf);
+route.post("/upload", upload.single("file"), validatePDF, insertPdf);
 
 export default route;
