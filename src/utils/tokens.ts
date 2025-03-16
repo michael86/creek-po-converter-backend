cd934fd93d6900341c72302b177cd693ff7b68de
michael86 <michael8t6@gmail.com>
2025-03-16 20:24:07 +0000

    fixed a cookies same site issue

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -3,6 +3,8 @@ import { Response } from "express";
 
 type Payload = Record<string, any>;
 
+const isProduction = process.env.NODE_ENV === "production";
+console.log(isProduction);
 export const generateToken = (payload: Payload) =>
   jwt.sign(payload, process.env.JWT_KEY!, { expiresIn: 60 * 60 * 6 });
 
@@ -10,6 +12,8 @@ export const setJwtCookie = (res: Response, token: string) => {
   res.cookie("authCookie", token, {
     maxAge: 1000 * 60 * 60 * 6, // 6 hours
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: isProduction, // False in development (localhost is HTTP)
+    sameSite: isProduction ? "none" : "lax", // "lax" allows cross-site on HTTP localhost
+    path: "/",
   });
 };
